5427564db6f3fa742c156e50d333f62aaf63a46f
Gregorio <gre208981@gmail.com>
2025-03-15 20:56:35 -0400

    Add nav styles and implement use of react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,19 @@
-import { useAuth0 } from '@auth0/auth0-react'
 import './App.css'
-import Login from './components/Login'
-import Logout from './components/Logout'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import Home from './components/home/Home'
+import Nav from './components/Nav'
 import Profile from './components/Profile'
 
 function App() {
-  const { isAuthenticated } = useAuth0()
-
   return (
     <>
-      <h1>
-        {isAuthenticated ? 'Interactive Comments Section' : 'Welcome to ICS!'}
-      </h1>
-
-      <main>
-        {isAuthenticated ? (
-          <>
-            <Profile />
-            <Logout />
-          </>
-        ) : (
-          <Login />
-        )}
-      </main>
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/profile' element={<Profile />} />
+        </Routes>
+      </Router>
     </>
   )
 }
